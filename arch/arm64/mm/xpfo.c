beb0d6349606c9f517098414c151492858446d63
Juerg Haefliger <juerg.haefliger@hpe.com>
2017-05-08 14:24:39 +0200

    arm64/mm: Add support for XPFO to swiotlb
    Pages that are unmapped by XPFO need to be mapped before and unmapped
    again after (to restore the original state) the __dma_{map,unmap}_area()
    operations to prevent fatal page faults.
    
    Signed-off-by: Juerg Haefliger <juerg.haefliger@canonical.com>
    Signed-off-by: Tycho Andersen <tycho@docker.com>

--- arch/arm64/mm/xpfo.c
+++ arch/arm64/mm/xpfo.c
@@ -11,8 +11,10 @@
  * the Free Software Foundation.
  */
 
+#include <linux/highmem.h>
 #include <linux/mm.h>
 #include <linux/module.h>
+#include <linux/xpfo.h>
 
 #include <asm/tlbflush.h>
 
@@ -61,4 +63,47 @@ inline void xpfo_flush_kernel_page(struct page *page, int order)
 	unsigned long size = PAGE_SIZE;
 
 	flush_tlb_kernel_range(kaddr, kaddr + (1 << order) * size);
+}
+
+inline void xpfo_dma_map_unmap_area(bool map, const void *addr, size_t size,
+				    int dir)
+{
+	unsigned long flags;
+	struct page *page = virt_to_page(addr);
+
+	/*
+	 * +2 here because we really want
+	 * ceil(size / PAGE_SIZE), not floor(), and one extra in case things are
+	 * not page aligned
+	 */
+	int i, possible_pages = size / PAGE_SIZE + 2;
+	void *buf[possible_pages];
+
+	memset(buf, 0, sizeof(void *) * possible_pages);
+
+	local_irq_save(flags);
+
+	/* Map the first page */
+	if (xpfo_page_is_unmapped(page))
+		buf[0] = kmap_atomic(page);
+
+	/* Map the remaining pages */
+	for (i = 1; i < possible_pages; i++) {
+		if (page_to_virt(page + i) >= addr + size)
+			break;
+
+		if (xpfo_page_is_unmapped(page + i))
+			buf[i] = kmap_atomic(page + i);
+	}
+
+	if (map)
+		__dma_map_area(addr, size, dir);
+	else
+		__dma_unmap_area(addr, size, dir);
+
+	for (i = 0; i < possible_pages; i++)
+		if (buf[i])
+			kunmap_atomic(buf[i]);
+
+	local_irq_restore(flags);
 }
