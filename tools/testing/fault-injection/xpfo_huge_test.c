39ca51d908b7738fecf612fabd81f2c12381e392
Marco Benatto <mbenatto@redhat.com>
2017-06-27 17:02:33 -0400

    Fixing compilation errors on xpfo_huge_test.c application
    Signed-off-by: Marco Benatto <mbenatto@redhat.com>

--- tools/testing/fault-injection/xpfo_huge_test.c
+++ tools/testing/fault-injection/xpfo_huge_test.c
@@ -6,23 +6,25 @@
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/mman.h>
 
 #define DEV "/dev/xpfoht"
 #define XPFO_IOCTL_READ 6969
 
 
 int main() {
-        int fd, ret = 1, size = getpagesize();
-        char *b;
+    int flags, fd, ret = 1, size = getpagesize();
+    char *b;
 
-        fd = open(DEV, O_RDWR);
-        if (fd == -1) {
+    fd = open(DEV, O_RDWR);
+    if (fd == -1) {
 		perror("open");
-                return -1;
-        }
+        return -1;
+    }
 
 	flags = MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB;
 	b = mmap(NULL, size * 20, PROT_WRITE | PROT_READ, flags, 0, 0);
+	printf("addr: %p\n", b);
 	if (b == MAP_FAILED) {
 		perror("mmap");
 		goto out_c;
@@ -31,17 +33,20 @@ int main() {
 	/* make sure the page is actually used */
 	b[0] = 0;
 
-        ret = ioctl(fd, XPFO_IOCTL_READ, b);
-        if (ret < 0) {
-                printf("Error on ioctl: %d\n", errno);
-                goto out_um;
-        }
+	system("grep -i Hugepage /proc/meminfo");
 
-        printf("ioctl returned: %d\n", ret);
+    ret = ioctl(fd, XPFO_IOCTL_READ, b);
+    if (ret < 0) {
+   		printf("Error on ioctl: %d\n", errno);
+        goto out_um;
+    }
+
+
+    printf("ioctl returned: %d\n", ret);
 	ret = 0;
 
 out_um:
-        munmap(b);
+        munmap(b, size*20);
 out_c:
         close(fd);
         return ret;
