bfb7098071102904c7cda68c9511e131f874b068
Tycho Andersen <tycho@docker.com>
2017-06-27 15:16:06 -0600

    fix tabs/spaces
    Signed-off-by: Tycho Andersen <tycho@docker.com>

--- tools/testing/fault-injection/xpfo_huge_test.c
+++ tools/testing/fault-injection/xpfo_huge_test.c
@@ -13,14 +13,14 @@
 
 
 int main() {
-    int flags, fd, ret = 1, size = getpagesize();
-    char *b;
+	int flags, fd, ret = 1, size = getpagesize();
+	char *b;
 
-    fd = open(DEV, O_RDWR);
-    if (fd == -1) {
+	fd = open(DEV, O_RDWR);
+	if (fd == -1) {
 		perror("open");
-        return -1;
-    }
+		return -1;
+	}
 
 	flags = MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB;
 	b = mmap(NULL, size * 20, PROT_WRITE | PROT_READ, flags, 0, 0);
@@ -35,19 +35,19 @@ int main() {
 
 	system("grep -i Hugepage /proc/meminfo");
 
-    ret = ioctl(fd, XPFO_IOCTL_READ, b);
-    if (ret < 0) {
-   		printf("Error on ioctl: %d\n", errno);
-        goto out_um;
-    }
+	ret = ioctl(fd, XPFO_IOCTL_READ, b);
+	if (ret < 0) {
+		printf("Error on ioctl: %d\n", errno);
+		goto out_um;
+	}
 
 
-    printf("ioctl returned: %d\n", ret);
+	printf("ioctl returned: %d\n", ret);
 	ret = 0;
 
 out_um:
-        munmap(b, size*20);
+	munmap(b, size*20);
 out_c:
-        close(fd);
-        return ret;
+	close(fd);
+	return ret;
 }
